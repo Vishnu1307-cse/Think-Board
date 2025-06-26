c4cc45c6923c9c49044a1fdd7b47431d945d031b
Vishnu <svvishnu33@gmail.com>
2025-06-26 20:59:14 +0530

    getNotesById

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -10,6 +10,19 @@ export async function getAllNotes(req,res) {
     }
 }
 
+export async function getNoteById(req, res) {
+    try {
+        const note = await Note.findById(req.params.id)
+        if (!note){
+            return res.status(404).json({message: "Note not found"})
+        }
+        res.status(200).json(note);
+    } catch (error) {
+        return res.status(500).json({message: "Internal server error", error: error.message});
+        console.error("Error fetching note by ID:", error);
+    }
+}
+
 export async function createNote(req,res) {
     try {
         const { title, content } = req.body;
